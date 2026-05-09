ad56c17f4dd5c91fc06f55a66873bc61c78e068d
WoldekirosM <101722084+WoldekirosM@users.noreply.github.com>
2026-05-08 22:24:50 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
+// Welcome player and collect their name
+let playerName = prompt("Welcome to Bear Ninja Hunter! Please enter your name:");
 
+// Greet player
+alert("Hello " + playerName + "! Let's play Bear Ninja Hunter!");
+
+// Ask for player's choice
+let playerChoice = prompt("Who are you: Bear, Ninja, or Hunter?");
+
+// Computer choice is hardcoded
+const computerChoice = "Bear";
+
+// Create result message
+let gameMessage =
+playerName + ", you chose " + playerChoice +
+". The computer chose " + computerChoice + ".";
+
+// Print to console
+console.log(gameMessage);
+
+// Print to webpage
+document.getElementById("gameOutput").innerHTML = gameMessage;
